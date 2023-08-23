38ecde6dde73312debdc30547fe70ada5bbc6ecb
NatalieJoan <nhelak2001@gmail.com>
2023-08-23 22:09:36 +0200

    Created Event Management Project in Laravel 10

---
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Event;
+use App\Models\Attendee;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+
+class AttendeeController extends Controller
+{
+    public function index(Event $event)
+    {
+        $attendees = $event->attendees()->latest();
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
+    }
+
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $event->attendees()->create([
+            'user_id' => 1
+        ]);
+
+        return new AttendeeResource($attendee);
+    }
+
+    public function show(Event $event, Attendee $attendee)
+    {
+        return new AttendeeResource($attendee);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $event, Attendee $attendee)
+    {
+        $attendee->delete();
+
+        return response(status: 204);
+    }
+}
