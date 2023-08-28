aeba0de7bd6c46e82e182f293e8c79a50c7eb125
NatalieJoan <nhelak2001@gmail.com>
2023-08-28 16:35:55 +0200

    Updated

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -18,6 +18,9 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'destroy']);
+        $this->authorizeResource(Attendee::class, 'attendee');
     }
 
     public function index(Event $event)
@@ -34,7 +37,7 @@ class AttendeeController extends Controller
     public function store(Request $request, Event $event)
     {
         $attendee = $event->attendees()->create([
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new AttendeeResource($attendee);
@@ -45,14 +48,8 @@ class AttendeeController extends Controller
         return new AttendeeResource($attendee);
     }
 
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
     public function destroy(Event $event, Attendee $attendee)
     {
-        $this->authorize('delete-attendee', [$event, $attendee]);
         $attendee->delete();
 
         return response(status: 204);
